6167b3d3b74ec91b8f226c24d4ce961c371f7ada
sarrionandia <r@sarrionandia.com>
2015-11-22 15:33:54 +0000

    Test for judges string method

---
+++ tab/test/testJudge.py
@@ -0,0 +1,14 @@
+from tab.models import Institution, Judge
+from django.test import TestCase
+
+
+class TeamTestCase(TestCase):
+
+    def setUp(self):
+        Institution.objects.create(name='University')
+        Judge.objects.create(name="Judge Judy", institution = Institution.objects.get(name='University'))
+
+    def test_return_string(self):
+        expected = 'Judge Judy <University>'
+        actual = Judge.objects.get(name='Judge Judy').__str__()
+        self.assertEqual(expected, actual)
