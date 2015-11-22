dceb7353c6bf9984522486ed9c772c2c03f73d85
sarrionandia <r@sarrionandia.com>
2015-11-22 16:24:48 +0000

    Validation for max number of speakers per term

--- tab/models.py
+++ tab/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
@@ -22,6 +23,11 @@ class Speaker(models.Model):
 
     def __str__(self):
         return self.name + ' <' + self.team.__str__() + '>'
+
+    def clean(self):
+        num_in_team = Speaker.objects.filter(team=self.team).count()
+        if num_in_team >= 2:
+            raise ValidationError('There can only be two speakers per team')
 
 class Judge(models.Model):
     name = models.CharField(max_length=80)
