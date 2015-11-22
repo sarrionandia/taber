e5143834ac88bdc8d2c7569ba99f23a74d041bd4
sarrionandia <r@sarrionandia.com>
2015-11-22 17:59:51 +0000

    Remove comments

--- tab/models.py
+++ tab/models.py
@@ -2,8 +2,6 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.core.validators import MinValueValidator
 
-# Create your models here.
-
 class Institution(models.Model):
     name = models.CharField(max_length=50);
 
