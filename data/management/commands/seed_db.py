79bd2021608aa6a69999df7fb0c66ec3cf6a5922
sarrionandia <r@sarrionandia.com>
2015-11-23 22:06:05 +0000

    Seed data for judges

--- data/management/commands/seed_db.py
+++ data/management/commands/seed_db.py
@@ -1,16 +1,29 @@
-from data.models import Institution
+from data.models import Institution, Judge
 from django.core.management.base import NoArgsCommand
+from random import randint
 
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
 
+        Judge.objects.all().delete()
         Institution.objects.all().delete()
 
 
         for i in institutions:
             institution = Institution(name=i)
             institution.save()
+
+        for n in names:
+            judge = Judge()
+            judge.institution = self.random_institution()
+            judge.name = n
+            judge.save()
+
+
+    def random_institution(self):
+        return  Institution.objects.order_by('?').first()
+
 
 institutions = [
     'Abertay University',
@@ -38,4 +51,37 @@ institutions = [
     'UCL',
     'University of Oxford',
     'University of Cambridge'
+]
+
+names = [
+    'Kaylee Wengerd',
+    'Dania Sprung',
+    'Joannie Busby',
+    'Tammera Begum',
+    'Aura Eudy',
+    'Candida Champagne',
+    'Domitila Dufresne',
+    'Rosalyn Durrah',
+    'See Diez',
+    'Carlota Sheller',
+    'Lanell Cremeans',
+    'Angelic Ester',
+    'Neda Iverson',
+    'Eunice Chiodo',
+    'Pansy Mackenzie',
+    'Cole Claxton',
+    'Suellen Cooperman',
+    'Elisa Trentham',
+    'Gregorio Middlebrooks',
+    'Jung Siers',
+    'Altha Marrin',
+    'Sunni Nantz',
+    'Loan Izzard',
+    'Cinda Damiani',
+    'Kellee Domingue',
+    'Dominic Purves',
+    'Denese Mcgibbon',
+    'Geraldo Flecha',
+    'Shay Sarinana',
+    'Trula Forbus',
 ]
