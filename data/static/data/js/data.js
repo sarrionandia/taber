8af2c4fca63bd11396bd13c9c7464224ff57024e
sarrionandia <r@sarrionandia.com>
2015-11-29 21:13:42 +0000

    Handling for 404 in ajax

--- data/static/data/js/data.js
+++ data/static/data/js/data.js
@@ -3,10 +3,22 @@ $( document ).ready(function() {
 });
 
 function delete_inst(inst_id) {
-    $.post("/data/institution/" + inst_id + "/delete/",
+    $('#inst_' + inst_id).find('button').attr('disabled', true);
+
+    var request = $.post("/data/institution/" + inst_id + "/delete/",
         {institution: inst_id},
         function() {
+            })
+
+        .fail(function(){
+            alert('Unable to delete institution ' + inst_id);
+            $('#inst_' + inst_id).find('button').attr('disabled', false);
+        })
+
+        .success(function() {
             $('#inst_' + inst_id).toggle({ effect: "scale", direction: "vertical" });
-            });
+        });
+
+
 
     }
