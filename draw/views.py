e54d82575cffef8272ab188d757edfb1aa0be718
sarrionandia <r@sarrionandia.com>
2015-11-30 21:51:04 +0000

    Draw console page

--- draw/views.py
+++ draw/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.views.generic import View
 
+from data.models import Team
+
+
 class DrawControl(View):
     def get(self, request):
         template = loader.get_template('draw/index.html')
-        context = RequestContext(request, {
 
+        team_count = Team.objects.all().count()
+
+        context = RequestContext(request, {
+            'teams_count' : Team.objects.all().count(),
+            'teams_ok' : (team_count % 4 == 0) and (team_count >= 4),
+            'rooms' : team_count / 4,
         })
         return HttpResponse(template.render(context))
