3031a2ce21f041fc5d38313825e3d9f1bb10747e
sarrionandia <r@sarrionandia.com>
2015-11-30 12:11:09 +0000

    URL for creating teams

--- data/urls.py
+++ data/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from . import views
-from views import DeleteInstitutionView, CreateInstitutionView, UpdateInstitutionView, DeleteTeamView
+from views import DeleteInstitutionView, CreateInstitutionView, UpdateInstitutionView, DeleteTeamView, CreateTeamView
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^institution/create/$', CreateInstitutionView.as_view(), name="create_institution"),
     url(r'^institution/(?P<institutionid>\d+)/update/$', UpdateInstitutionView.as_view(), name='update_institution'),
     url(r'^team/(?P<teamid>\d+)/delete/$', DeleteTeamView.as_view(), name='delete_team'),
+    url(r'^team/create/$', CreateTeamView.as_view(), name='create_team'),
 ]
