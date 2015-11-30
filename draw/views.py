8439833f3f9bae6892b856ce633d339e195f827c
sarrionandia <r@sarrionandia.com>
2015-11-30 21:30:56 +0000

    Template for draw

--- draw/views.py
+++ draw/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import RequestContext, loader
 from django.views.generic import View
 
 class DrawControl(View):
     def get(self, request):
-        return HttpResponse("OK")
+        template = loader.get_template('draw/index.html')
+        context = RequestContext(request, {
+
+        })
+        return HttpResponse(template.render(context))
