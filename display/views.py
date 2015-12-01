f188e80b7330a9234ecfd5c92849089885f73d31
sarrionandia <r@sarrionandia.com>
2015-12-01 10:33:12 +0000

    Table view for rounds

--- display/views.py
+++ display/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader, RequestContext
 from django.views.generic import View
+
+from draw.models import Debate
 
 
 class DrawTableView(View):
 
     def get(self, request, round):
-        return HttpResponse("OK")
+        template = loader.get_template('display/table.html')
+        context = RequestContext(request, {
+            'round' : round,
+            'debates' : Debate.objects.all().filter(round=round)
+        })
+        return HttpResponse(template.render(context))
