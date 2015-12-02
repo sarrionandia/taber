9750c1de35f997c0cafa32cd3a1c0b41e9aa0e0a
sarrionandia <r@sarrionandia.com>
2015-12-02 10:10:50 +0000

    Test for max speaker score

--- results/test/testModel/testResult.py
+++ results/test/testModel/testResult.py
@@ -13,3 +13,11 @@ class ResultTestCase(TestCase):
         result.ogsp2 = -2
         with self.assertRaises(ValidationError):
             result.full_clean()
+
+    def testMaxSpeakerScore(self):
+        result = generate_objects.valid_result_with_debate()
+
+        result.ogsp1 = 101
+
+        with self.assertRaises(ValidationError):
+            result.full_clean()
