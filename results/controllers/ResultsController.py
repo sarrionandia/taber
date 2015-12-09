935cc55847614727d60aefbad089e454833e3d56
sarrionandia <r@sarrionandia.com>
2015-12-09 22:21:52 +0000

    Exception message

--- results/controllers/ResultsController.py
+++ results/controllers/ResultsController.py
@@ -3,7 +3,7 @@ from draw.models import TournamentStateException, Tournament, Debate
 
 def results_entered_for_round(round):
     if round > Tournament.instance().round:
-        raise TournamentStateException
+        raise TournamentStateException("Not all results are input for this round")
 
     for debate in Debate.objects.filter(round=round):
         if not debate.has_result:
