bc066923c9d604f64e7dd003210062c25ad999c5
sarrionandia <r@sarrionandia.com>
2015-12-15 20:08:57 +0000

    Create blank pools

--- draw/controller/DrawController.py
+++ draw/controller/DrawController.py
@@ -1,14 +1,28 @@
 from draw.models import Tournament, TournamentStateException
+from results.controllers.PointsController import PointsController
 from results.controllers.ResultsController import ResultsController
 
 
 class DrawController():
 
-    resultsController = ResultsController()
+    resultsController = None
+    pointsController = None
 
-    def create_pools(self):
+    def __init__(self):
+        self.resultsController = ResultsController()
+        self.pointsController = PointsController()
+
+    def create_pools(self, teams, max_round):
         if not self.resultsController.results_entered_for_round(Tournament.instance().round):
             raise TournamentStateException("All results for current round must be entered to draw")
 
         return []
 
+    def create_blank_pools(self, max_round):
+        pools = {}
+        if max_round == 0:
+            return pools
+
+        for points in range(0, max_round*4):
+            pools.update({points:[]})
+        return pools
--- draw/test/test_controller/test_draw.py
+++ draw/test/test_controller/test_draw.py
@@ -2,25 +2,23 @@ from django.test import TestCase
 
 from data.test import generate_objects
 from draw.controller.DrawController import DrawController
-from draw.models import TournamentStateException, Tournament
-
-
-class Teams(object):
-    pass
+from draw.models import TournamentStateException
 
 
 class DrawControllerTestCase(TestCase):
-    def testCreatePoolsWithEmptyList(self):
-        controller = DrawController()
-        self.assertEqual(0, len(controller.create_pools()))
 
-    def testCreatePoolsWithNone(self):
-        controller = DrawController()
-        self.assertEqual(0, len(controller.create_pools()))
+    def setUp(self):
+        self.controller = DrawController()
 
     def testThrowsExceptionIfNotAllDebatesHaveResult(self):
         generate_objects.setup_IV_R1()
         with self.assertRaises(TournamentStateException):
-            controller = DrawController()
-            controller.create_pools()
+            self.controller.create_pools([], 3)
 
+    def testCreateBlankPoolsReturnsEmptyForRoundZero(self):
+        pools = self.controller.create_blank_pools(0)
+        self.assertEqual(0, len(pools.keys()), "Should not create any pools for round zero")
+
+    def testCreateBlankPoolsReturnsCorrectNumberOfPoolsForR1(self):
+        pools = self.controller.create_blank_pools(1)
+        self.assertEqual(4, len(pools.keys()), "Should be four possible pools for Round 1")
