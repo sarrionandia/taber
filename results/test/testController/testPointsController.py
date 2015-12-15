07a2fd661b0cad6239cb986057a431b588baac12
sarrionandia <r@sarrionandia.com>
2015-12-15 12:14:31 +0000

    Test interaction between points controller and tresults controller

--- results/test/testController/testPointsController.py
+++ results/test/testController/testPointsController.py
@@ -11,23 +11,21 @@ class PointsControllerTestCase(TestCase):
         self.oo = Mock()
         self.cg = Mock()
         self.co = Mock()
-
         self.points_controller = PointsController()
-        results_controller = Mock()
-
+        self.results_controller = Mock()
         debate = Mock(OG=self.og, OO=self.oo, CG=self.cg, CO=self.co)
         result = Mock(debate=debate, og=0, oo=1, cg=2, co=3)
-
-        results_controller.result_for_team.return_value = result
-        self.points_controller.results_controller = results_controller
-
+        self.results_controller.result_for_team.return_value = result
+        self.points_controller.results_controller = self.results_controller
 
 
     def testGetTeamPoints(self):
-
-
         self.assertEqual(0, self.points_controller.team_points_for_team(self.og, 1), "Did not get correct team score for OG")
         self.assertEqual(1, self.points_controller.team_points_for_team(self.oo, 1), "Did not get correct team score for OO")
         self.assertEqual(2, self.points_controller.team_points_for_team(self.cg, 1), "Did not get correct team score for CG")
         self.assertEqual(3, self.points_controller.team_points_for_team(self.co, 1), "Did not get correct team score for CO")
 
+
+    def testGetTeamPointsCallsResultController(self):
+        self.points_controller.team_points_for_team(self.og, 1)
+        self.results_controller.result_for_team.assert_called_once_with(self.og, 1)
