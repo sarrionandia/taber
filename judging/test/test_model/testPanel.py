28b12f7784309bc8df277656b5e39b0c90430a64
sarrionandia <r@sarrionandia.com>
2016-01-02 15:32:01 +0000

    Test for valid panel

--- judging/test/test_model/testPanel.py
+++ judging/test/test_model/testPanel.py
@@ -7,6 +7,21 @@ from judging.models import Panel
 
 class PanelTestCase(TestCase):
 
+    def testAllowsValidPanel(self):
+        judges = [generate_objects.valid_judge(), generate_objects.valid_judge(), generate_objects.valid_judge()]
+        chair = judges[1]
+        debate = generate_objects.valid_debate()
+        debate.save()
+
+        panel = Panel(chair=chair)
+        panel.debate = debate
+        panel.save()
+
+        panel.judges.add(judges[0], judges[1], judges[2])
+
+        panel.full_clean()
+        panel.save()
+
     def testChairNotInPanel(self):
         judges = [generate_objects.valid_judge(), generate_objects.valid_judge(), generate_objects.valid_judge()]
         chair = generate_objects.valid_judge()
